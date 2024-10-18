d127a67b02ece8758d4e509d7318444c88f11308
Shridhar sharma <shridhar6200@gmail.com>
2024-10-18 07:52:09 +0530

    add multiply

--- coding.js
+++ coding.js
@@ -8,10 +8,11 @@ const result=add(5,6);
 console.log(result);
 
 
+function multiply(a ,b,c)
+{
+    return a*b*c;
+}
 
 
-
-
-
-const result2=multiply(5,6);
-console.log(result);
+const result2=multiply(5,6,7);
+console.log(result2);
